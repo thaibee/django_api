1064dfcae9c461b6abe7c63627ded5ec77448be3
thaibee <doctor.madbee@gmail.com>
2022-08-18 07:40:50 +0700

    add serializers.py install rest_framework add view for goods

--- api/urls.py
+++ api/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from izoor.views import GoodsApiView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/goodsList/', GoodsApiView.as_view())
 ]
--- izoor/views.py
+++ izoor/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import generics
 # Create your views here.
+from izoor.models import Goods
+from izoor.serializers import GoodsSerializer
+
+
+class GoodsApiView(generics.ListAPIView):
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
