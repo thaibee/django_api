42431121208907e1057242113e2f33d6ce181558
magadanthai <doctor.madbee@gmail.com>
2022-08-18 18:01:07 +0700

    playing with serializers.py

--- izoor/serializers.py
+++ izoor/serializers.py
@@ -1,10 +1,45 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from izoor.models import Goods
 
+ # Первая версия сериализатора из модели
+# class GoodsSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Goods
+#         fields = ('barcode', 'name', 'category')
 
-class GoodsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Goods
-        fields = ('barcode', 'name', 'category')
 
+#Ручная сериализация
+# class GoodsModel:
+#     def __init__(self, barcode, name, price):
+#         self.barcode = barcode
+#         self.name = name
+#         self.price = price
+# class GoodsSerializer(serializers.Serializer):
+#     barcode = serializers.CharField(max_length=12)
+#     name = serializers.CharField(max_length=20)
+#     price = serializers.DecimalField(max_digits=12, decimal_places=4)
+# def encode():
+#     model = GoodsModel('123', 'banka supa', 20.1)
+#     model_sr = GoodsSerializer(model)
+#     print(model_sr.data, type(model_sr), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+# def decode():
+#     stream = io.BytesIO(b'{"barcode":"23425","name":"test4","price":"2"}')
+#     data = JSONParser().parse(stream)
+#     serializer = GoodsSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
+#     print(serializer.errors)
+
+class GoodSerializator(serializers.Serializer):
+    barcode = serializers.CharField(max_length=15)
+    name = serializers.CharField(max_length=50)
+    price = serializers.DecimalField(max_digits=19, decimal_places=4)
+    category_id = serializers.CharField(max_length=36)
+    supplier_id = serializers.CharField(max_length=36)
--- izoor/views.py
+++ izoor/views.py
@@ -1,16 +1,35 @@
+from uuid import UUID
+
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 # Create your views here.
 from izoor.models import Goods
-from izoor.serializers import GoodsSerializer
+from izoor.serializers import GoodSerializator
 
 
 # class GoodsApiView(generics.ListAPIView):
 #     queryset = Goods.objects.all()
 #     serializer_class = GoodsSerializer
 
+# стандартный метод вывода
 class GoodsApiView(APIView):
     def get(self, request):
-        return Response({'name': 'Любаня'})
+        goods = Goods.objects.all().values()
+        goods_serialized = GoodSerializator(goods, many=True).data
+        return Response({'posts': goods_serialized})
+
+    def post(self, request):
+        serializer = GoodSerializator(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # new_good = Goods.objects.create(
+        #     barcode=request.data['barcode'],
+        #     name=request.data['name'],
+        #     price=request.data['price'],
+        #     supplier_id=[UUID('supplier_id')],
+        #     category_id=[UUID('category_id')],
+        # )
+
+        return Response({'post': 'GoodSerializator(new_good).data'})
