df563774cbdb5358f495449a613d0dab4ba65834
thaibee <doctor.madbee@gmail.com>
2022-08-18 08:44:26 +0700

    add one more APIView

--- izoor/views.py
+++ izoor/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
 from izoor.models import Goods
 from izoor.serializers import GoodsSerializer
 
 
-class GoodsApiView(generics.ListAPIView):
-    queryset = Goods.objects.all()
-    serializer_class = GoodsSerializer
+# class GoodsApiView(generics.ListAPIView):
+#     queryset = Goods.objects.all()
+#     serializer_class = GoodsSerializer
+
+class GoodsApiView(APIView):
+    def get(self, request):
+        return Response({'name': 'Любаня'})
